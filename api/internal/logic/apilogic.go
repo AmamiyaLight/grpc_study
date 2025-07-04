ceda6c9d55df6077da4c9d8dd6998f5a3daab3cc
AmamiyaLight <2735585086@qq.com>
2025-07-04 18:30:33 +0800

    feat-gozero_demo

---
+++ api/internal/logic/apilogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"grpc_study/api/internal/svc"
+	"grpc_study/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ApiLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewApiLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ApiLogic {
+	return &ApiLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *ApiLogic) Api(req *types.Request) (resp *types.Response, err error) {
+	return &types.Response{
+		Message: "aaaa",
+	}, nil
+}
